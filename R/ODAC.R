eea493d92ecb04bf826caa16390ed0139621d778
Kunlong Liu <lonhuen@gmail.com>
2024-02-26 14:01:51 -0800

    remove trace

--- R/ODAC.R
+++ R/ODAC.R
@@ -284,7 +284,7 @@ ODAC.estimate <- function(ipdata, control, config) {
     fn = logL_tilde,
     # gr = logL_tilde_D1,
     hessian = TRUE,
-    control = list(maxit = control$optim_maxit, trace = 6)
+    control = list(maxit = control$optim_maxit)
   )
 
   surr <- list(btilde = sol$par, Htilde = sol$hessian, site = config$site_id, site_size = nrow(ipdata))
